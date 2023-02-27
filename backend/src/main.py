ff8f2eb3eec6d7818bc9f440973d5dbac0cf2e2d
AdrCor <adrien.corre1999@gmail.com>
2023-02-27 16:02:29 +0100

    Add deployment files for backend

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,12 +1,18 @@
 from flask import Flask
 from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+
+load_dotenv('.env')
 
 app = Flask(__name__)
 CORS(app)
+
+app.debug = os.getenv('ENVIRONMENT') == 'local'
 
 @app.route('/')
 def home():
     return 'Api is running'
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(port=5000)
