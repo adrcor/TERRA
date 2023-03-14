c407b8825a89d4599d792271dd1982723fac20e4
AdrCor <adrien.corre1999@gmail.com>
2023-03-14 23:45:05 +0100

    Change nav bar home to terra

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-row items-center justify-between h-16 w-full max-w-3xl px-6 py-12">
-        <router-link to="/" class="text-primary text-3xl">Home</router-link>
+        <router-link to="/" class="text-primary text-3xl">Terra</router-link>
         <router-link to="account" class="text-on-background text-2xl">{{ store.state.auth.username ? store.state.auth.username : 'Login' }}</router-link>
     </div>
 </template>
