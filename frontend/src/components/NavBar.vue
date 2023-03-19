194b00ed79f2c309858cbc89c9cfdf1e05672d2b
AdrCor <adrien.corre1999@gmail.com>
2023-03-19 21:23:29 +0100

    Fix nav bar routing

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-row items-center justify-between h-16 w-full max-w-3xl px-6 py-12">
-        <router-link to="/" @click.native="router.go(0)" class="text-primary text-3xl">Terra</router-link>
+        <button @click.native="onClick" class="text-primary text-3xl">Terra</button>
         <router-link to="account" class="text-on-background text-2xl">{{ store.state.auth.username ? store.state.auth.username : 'Login' }}</router-link>
     </div>
 </template>
@@ -12,5 +12,14 @@ import { useRouter } from 'vue-router';
 const store = useStore()
 
 const router = useRouter()
+
+function onClick() {
+    const currentRoute = router.currentRoute.value.fullPath
+    if (currentRoute == '/') {
+        router.go(0)
+    } else {
+        router.push('/')
+    }
+}
 
 </script>
