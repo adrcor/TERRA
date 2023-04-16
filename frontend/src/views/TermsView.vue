71351dc5dd7701e3a5ea4ab2c725298a60ff17e6
AdrCor <adrien.corre1999@gmail.com>
2023-04-16 14:52:20 +0200

    Change Terra Trivia to TerraTrivia

--- frontend/src/views/TermsView.vue
+++ frontend/src/views/TermsView.vue
@@ -3,11 +3,11 @@
         <div class="flex flex-col gap-2">
             <h1 class="text-primary text-3xl underline text-center pb-4">Terms and Conditions</h1>
             
-            <p>Welcome to Terra Trivia!</p>
+            <p>Welcome to TerraTrivia!</p>
             
-            <p>These terms and conditions outline the rules and regulations for the use of Terra Trivia's Website, located at <a class="text-primary text-opacity-80" href="https://terratrivia.app">terratrivia.app</a>.</p>
+            <p>These terms and conditions outline the rules and regulations for the use of TerraTrivia's Website, located at <a class="text-primary text-opacity-80" href="https://terratrivia.app">terratrivia.app</a>.</p>
             
-            <p>By accessing this website we assume you accept these terms and conditions. Do not continue to use Terra Trivia if you do not agree to take all of the terms and conditions stated on this page.</p>
+            <p>By accessing this website we assume you accept these terms and conditions. Do not continue to use TerraTrivia if you do not agree to take all of the terms and conditions stated on this page.</p>
             
             <p>The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: "Client", "You" and "Your" refers to you, the person log on this website and compliant to the Company's terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client's needs in respect of provision of the Company's stated services, in accordance with and subject to, prevailing law of fr. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.</p>
         </div>
@@ -15,7 +15,7 @@
         <div class="flex flex-col gap-2">
             <h2 class="text-primary text-2xl">Cookies</h2>
             
-            <p>We employ the use of cookies. By accessing Terra Trivia, you agreed to use cookies in agreement with the Terra Trivia's Privacy Policy.</p>
+            <p>We employ the use of cookies. By accessing TerraTrivia, you agreed to use cookies in agreement with the TerraTrivia's Privacy Policy.</p>
             
             <p>Most interactive websites use cookies to let us retrieve the user's details for each visit. Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising partners may also use cookies.</p>
         </div>
@@ -23,21 +23,21 @@
         <div class="flex flex-col gap-2">
             <h2 class="text-primary text-2xl">License</h2>
             
-            <p>Unless otherwise stated, Terra Trivia and/or its licensors own the intellectual property rights for all material on Terra Trivia. All intellectual property rights are reserved. You may access this from Terra Trivia for your own personal use subjected to restrictions set in these terms and conditions.</p>
+            <p>Unless otherwise stated, TerraTrivia and/or its licensors own the intellectual property rights for all material on TerraTrivia. All intellectual property rights are reserved. You may access this from TerraTrivia for your own personal use subjected to restrictions set in these terms and conditions.</p>
             
             <p>You must not:</p>
             <ul class="px-3">
-                <li>- Republish material from Terra Trivia</li>
-                <li>- Sell, rent or sub-license material from Terra Trivia</li>
-                <li>- Reproduce, duplicate or copy material from Terra Trivia</li>
-                <li>- Redistribute content from Terra Trivia</li>
+                <li>- Republish material from TerraTrivia</li>
+                <li>- Sell, rent or sub-license material from TerraTrivia</li>
+                <li>- Reproduce, duplicate or copy material from TerraTrivia</li>
+                <li>- Redistribute content from TerraTrivia</li>
             </ul>
             
             <p>This Agreement shall begin on the date hereof.</p>
             
-            <p>Parts of this website offer an opportunity for users to post and exchange opinions and information in certain areas of the website. Terra Trivia does not filter, edit, publish or review Comments prior to their presence on the website. Comments do not reflect the views and opinions of Terra Trivia,its agents and/or affiliates. Comments reflect the views and opinions of the person who post their views and opinions. To the extent permitted by applicable laws, Terra Trivia shall not be liable for the Comments or for any liability, damages or expenses caused and/or suffered as a result of any use of and/or posting of and/or appearance of the Comments on this website.</p>
+            <p>Parts of this website offer an opportunity for users to post and exchange opinions and information in certain areas of the website. TerraTrivia does not filter, edit, publish or review Comments prior to their presence on the website. Comments do not reflect the views and opinions of TerraTrivia,its agents and/or affiliates. Comments reflect the views and opinions of the person who post their views and opinions. To the extent permitted by applicable laws, TerraTrivia shall not be liable for the Comments or for any liability, damages or expenses caused and/or suffered as a result of any use of and/or posting of and/or appearance of the Comments on this website.</p>
             
-            <p>Terra Trivia reserves the right to monitor all Comments and to remove any Comments which can be considered inappropriate, offensive or causes breach of these Terms and Conditions.</p>
+            <p>TerraTrivia reserves the right to monitor all Comments and to remove any Comments which can be considered inappropriate, offensive or causes breach of these Terms and Conditions.</p>
             
             <p>You warrant and represent that:</p>
             
@@ -48,7 +48,7 @@
                 <li>- The Comments will not be used to solicit or promote business or custom or present commercial activities or unlawful activity</li>
             </ul>
             
-            <p>You hereby grant Terra Trivia a non-exclusive license to use, reproduce, edit and authorize others to use, reproduce and edit any of your Comments in any and all forms, formats or media.</p>
+            <p>You hereby grant TerraTrivia a non-exclusive license to use, reproduce, edit and authorize others to use, reproduce and edit any of your Comments in any and all forms, formats or media.</p>
         </div>
         
         <div class="flex flex-col gap-2">
@@ -78,11 +78,11 @@
                 <li>- educational institutions and trade associations</li>
             </ul>
             
-            <p>We will approve link requests from these organizations if we decide that: (a) the link would not make us look unfavorably to ourselves or to our accredited businesses; (b) the organization does not have any negative records with us; (c) the benefit to us from the visibility of the hyperlink compensates the absence of Terra Trivia; and (d) the link is in the context of general resource information.</p>
+            <p>We will approve link requests from these organizations if we decide that: (a) the link would not make us look unfavorably to ourselves or to our accredited businesses; (b) the organization does not have any negative records with us; (c) the benefit to us from the visibility of the hyperlink compensates the absence of TerraTrivia; and (d) the link is in the context of general resource information.</p>
             
             <p>These organizations may link to our home page so long as the link: (a) is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement or approval of the linking party and its products or services; and (c) fits within the context of the linking party's site.</p>
             
-            <p>If you are one of the organizations listed in paragraph 2 above and are interested in linking to our website, you must inform us by sending an e-mail to Terra Trivia. Please include your name, your organization name, contact information as well as the URL of your site, a list of any URLs from which you intend to link to our Website, and a list of the URLs on our site to which you would like to link. Wait 2-3 weeks for a response.</p>
+            <p>If you are one of the organizations listed in paragraph 2 above and are interested in linking to our website, you must inform us by sending an e-mail to TerraTrivia. Please include your name, your organization name, contact information as well as the URL of your site, a list of any URLs from which you intend to link to our Website, and a list of the URLs on our site to which you would like to link. Wait 2-3 weeks for a response.</p>
             
             <p>Approved organizations may hyperlink to our Website as follows:</p>
             
@@ -92,7 +92,7 @@
                 <li>- By use of any other description of our Website being linked to that makes sense within the context and format of content on the linking party's site</li>
             </ul>
             
-            <p>No use of Terra Trivia's logo or other artwork will be allowed for linking absent a trademark license agreement.</p>
+            <p>No use of TerraTrivia's logo or other artwork will be allowed for linking absent a trademark license agreement.</p>
         </div>
         
         <div class="flex flex-col gap-2">
